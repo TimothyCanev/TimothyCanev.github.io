81eb8c41ae17577b4ed531a14113796c6daaf886
TimothyCanev <tican23@student.sdu.dk>
2024-02-23 23:54:07 +0100

    fea

--- scripthelper.js
+++ scripthelper.js
@@ -1,2 +1,3 @@
-const element = document.getElementById('centeredElement');
+const element = document.getElementById('topText');
 element.style.textAlign = 'center';
+element.style.fontWeight = 300;
