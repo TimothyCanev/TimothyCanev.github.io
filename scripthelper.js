19cc81a9abf18d632b6471e9d34b2ab4e1d892b1
TimothyCanev <tican23@student.sdu.dk>
2024-02-23 23:47:43 +0100

    dwa

--- scripthelper.js
+++ scripthelper.js
@@ -1,13 +1,2 @@
 const element = document.getElementById('centeredElement');
 element.style.textAlign = 'center';
-
-// Smooth scrolling functionality
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
