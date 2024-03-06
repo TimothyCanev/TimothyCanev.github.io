237586effe59cf5116430b73b3d6ad1c311987bf
TimothyCanev <tican23@student.sdu.dk>
2024-03-06 15:26:20 +0100

    wednesday top line CommitnPush

--- scripthelper.js
+++ scripthelper.js
@@ -1,3 +1,8 @@
 const element = document.getElementById('topText');
 //element.style.textAlign = 'center';
 element.style.wordSpacing = 25;
+
+window.addEventListener('scroll', function() {
+    var topLine = document.querySelector('#topLine');
+    topLine.style.top = window.pageYOffset + 'px';
+  });
