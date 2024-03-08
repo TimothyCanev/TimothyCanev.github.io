af31fee693c73d992034334c8225bc59e2e527db
TimothyCanev <tican23@student.sdu.dk>
2024-03-08 14:15:04 +0100

    FridayComittNPush

--- scripthelper.js
+++ scripthelper.js
@@ -1,8 +1,3 @@
 const element = document.getElementById('topText');
 //element.style.textAlign = 'center';
 element.style.wordSpacing = 25;
-
-window.addEventListener('scroll', function() {
-    var topLine = document.querySelector('#topLine');
-    topLine.style.top = window.pageYOffset + 'px';
-  });
